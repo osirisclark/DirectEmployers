d1e674f0d7c2aeb404ebd5c8ea54c21a6db94209
OsirisMango <osirisgclark@gmail.com>
2018-09-20 22:38:01 -0400

    Direct Employers problem 2

---
+++ directemployers2.py
@@ -0,0 +1,35 @@
+
+"""
+Write a function that accepts a string and returns that string in reverse order
+"""
+
+x = 'DirectEmployers'
+
+"""
+using slice notation
+"""
+print(x[::-1])
+
+"""
+# using reversed() function
+"""
+
+
+# def reversed_str(x):
+#     return ''.join(reversed(x))
+
+# print(reversed_str(x))
+
+"""
+# Using recursion
+"""
+
+
+# def reverse(x):
+#     if len(x) == 0:
+#         return x
+#     else:
+#         return reverse(x[1:]) + x[0]
+
+
+# print(reverse(x))
